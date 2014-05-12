e958c4044991a41ca27cd4d43dd85064c60f7eef
grynmax <grynmax@gmail.com>
2014-05-13 02:16:18 +0300

    first commit

---
+++ PhalconModel.php
@@ -0,0 +1,18 @@
+<?php
+namespace PApp\Models;
+
+use PApp\Base\BaseModel;
+
+class $FILE$ extends BaseModel {
+    public function initialize()
+    {
+//        $this->setConnectionService('RemoteSphinx');
+    }
+
+    public function getSource()
+    {
+        return '$TABLE$';
+    }
+
+
+}
